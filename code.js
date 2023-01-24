ffa0c342d67886a4a2770c56ca306bc0c281037a
AnandKrishnanKS <anandakrishnan1000@gmail.com>
2023-01-25 01:29:06 +0530

    initial

--- code.js
+++ code.js
@@ -5,23 +5,73 @@ function getdata() {
 }
 function displaydata(placeDeails) {
     placeName = placeDeails.name
+    placeId = placeDeails.id
+    placeCode = placeDeails.cod
+    cordLon = placeDeails.coord.lon
+    cordLat = placeDeails.coord.lat
+    sunRise = placeDeails.sys.sunrise
+    sunSet = placeDeails.sys.sunset
     weatherType = placeDeails.weather[0].description
     weatherIcon = placeDeails.weather[0].icon
-    normTemp = (placeDeails.main.temp - 32) * 5 / 9
-    minTemp = placeDeails.main.temp_min
-    maxTemp = placeDeails.main.temp_max
+    normTemp = Math.round((placeDeails.main.temp - 32) * 5 / 9)
+    minTemp = Math.round((placeDeails.main.temp_min - 32) * 5 / 9)
+    maxTemp = Math.round((placeDeails.main.temp_max - 32) * 5 / 9)
     humidity = placeDeails.main.humidity
+    pressure = placeDeails.main.pressure
     windSpeed = placeDeails.wind.speed
     windDirection = placeDeails.wind.deg
 
-    result.innerHTML = `
-                 <div>
-                       <p>place name:${placeName}</p>
-                       <p>weather Type:${weatherType} ${weatherIcon}</p>
-                       <p>temprature Normal:${normTemp}&#8451; Min:${minTemp} Max:${maxTemp}</p>
-                       <p>humidity:${humidity}</p>
-                       <p>wind Speed:${windSpeed} Direction:${windDirection}</p>
-                </div>
+    result.innerHTML = ` 
+                 <style>.wlcmpg{
+                    opacity: 0;
+                }
+                 }</style>
+                 <div class="col-md-12 col-lg-5  flex-fill ">
+                 <p class="text-white ps-1"><strong>today's weather at</strong> </p>
+                 <h3 class="text-center text-primary mb-5"
+                     style="font-size: 3.5rem; font-weight: 900;">${placeName}
+                 </h3>
+                 <div class="d-flex flex-fill justify-content-around  text-white">
+                     <p class="bg-secondary bg-opacity-25 ps-3 pe-3 rounded-4 mb-4">Id : ${placeId}</p>
+                     <p class="bg-secondary bg-opacity-25 ps-3 pe-3 rounded-4 mb-4">location- 
+                         lon : ${cordLon} ,
+                         lat : ${cordLat}
+                     </p>
+                     <p class="bg-secondary bg-opacity-25 ps-3 pe-3 rounded-4 mb-4">Code : ${placeCode}</p>
+                 </div>
+                 <div class="d-flex flex-fill justify-content-evenly  text-white">
+                     <p class="bg-secondary bg-opacity-25 ps-3 pe-3 rounded-4 ms-5 mb-4">
+                         Sunrise : ${sunRise}
+                     </p>
+                     <p class="bg-secondary bg-opacity-25 ps-3 pe-3 rounded-4 me-5 mb-4">Sunset : ${sunSet}
+                     </p>
+                 </div>
+                 <div class="d-flex flex-fill justify-content-evenly  text-white">
+                     <p class="bg-secondary bg-opacity-25 ps-3 pe-3 rounded-4">Wind- speed :
+                         ${windSpeed} and
+                         Deg : ${windDirection}
+                     </p>
+
+                 </div>
+             </div>
+             <div class=" col-md-12 col-lg-3 flex-fill text-white">
+                 <p class=" p-0 m-0  ps-5"><strong>Temprature</strong> - avg:</p>
+                 <h1 class=" text-end me-4 " style="font-size: 6rem;">${normTemp}&#8451;
+                 </h1>
+                 <div class="d-flex">
+                     <p class=" flex-fill ps-5 ">Min :<strong> ${minTemp} &#8451;</strong>
+                     </p>
+                     <p class=" flex-fill text-end ">Max :<strong> ${maxTemp} &#8451;</strong> </p>
+                 </div>
+                 <div class="d-flex text-center">
+                     <p class=" flex-fill bg-success bg-opacity-25 ms-5 me-4 p-3 pe-3 rounded-4">
+                         Humidity : ${humidity}
+                     </p>
+                     <p class="flex-fill bg-success bg-opacity-25 p-3  me-0 rounded-4">Air pressure :
+                         ${pressure}
+                     </p>
+                 </div>
+             </div>
             `
 }
 
